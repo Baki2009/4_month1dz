7f6c46d48d37b563f98238a8731b7fdb456777fe
Baki2009 <bakajsamatov@gmail.com>
2023-12-08 17:09:40 +0600

    commit

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ load_dotenv(os.path.join(BASE_DIR,".env"))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
